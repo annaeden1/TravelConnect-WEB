d453569ed4c5257ab4f5aa385d0b58da74f4d515
Liam Hemo <liamhemo64@gmail.com>
2026-01-28 19:34:03 +0200

    feat(client): integrate login route and update styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,41 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
 import Navbar from "./components/Navbar";
 import Home from "./pages/HomePage";
 import AIAssistant from "./pages/AIAssistantPage";
 import CreatePost from "./pages/CreatePostPage";
 import Profile from "./pages/ProfilePage";
+import LoginPage from "./pages/LoginPage";
 import ClientRoutes from "./utils/appRoutes";
+import { AuthProvider } from "./context/AuthContext";
 
-const App = () => {
+const AppContent = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === ClientRoutes.LOGIN;
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route index path={ClientRoutes.HOME} element={<Home />} />
-          <Route path={ClientRoutes.AI} element={<AIAssistant />} />
-          <Route path={ClientRoutes.POST} element={<CreatePost />} />
-          <Route path={ClientRoutes.PROFILE} element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
+      {!isLoginPage && <Navbar />}
+      <Routes>
+        <Route index path={ClientRoutes.HOME} element={<Home />} />
+        <Route path={ClientRoutes.AI} element={<AIAssistant />} />
+        <Route path={ClientRoutes.POST} element={<CreatePost />} />
+        <Route path={ClientRoutes.PROFILE} element={<Profile />} />
+        <Route path={ClientRoutes.LOGIN} element={<LoginPage />} />
+      </Routes>
     </>
   );
 };
 
+const App = () => {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
 export default App;
+
