f7af6a7409340c7ff03612f572c29346dfede3b4
Liam Hemo <liamhemo64@gmail.com>
2026-02-24 01:57:03 +0200

    try to not crash

--- server/src/index.ts
+++ server/src/index.ts
@@ -57,9 +57,9 @@ const intApp = () => {
     app.use("/public", express.static("public"));
     app.use(express.static(path.join(__dirname, "../../public")));
 
-    app.get("(*)", (req, res) => {
+    app.get("/*", (req, res) => {
       res.sendFile(path.join(__dirname, "../../public", "index.html"));
-    });
+    }); 
 
     const dbUri = process.env.MONGODB_URI;
     if (!dbUri) {
