dc7a8875760de82f32f96319aef7af221ea651a2
Liam Hemo <liamhemo64@gmail.com>
2026-02-24 08:12:28 +0200

    according to claude

--- server/src/index.ts
+++ server/src/index.ts
@@ -57,7 +57,7 @@ const intApp = () => {
     app.use("/public", express.static("public"));
     app.use(express.static(path.join(__dirname, "../../public")));
 
-    app.get("*", (req, res) => {
+    app.get("/{*path}", (req, res) => {
       res.sendFile(path.join(__dirname, "../../public", "index.html"));
     }); 
 
